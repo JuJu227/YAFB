ebe079402c6bbebd0c504c0b7a644f9c49c18f1f
Vanessa Sergeon <vanessa.sergeon@magnetic.com>
2015-10-21 14:39:49 -0400

    models finished except for image field

---
+++ directory/migrations/0001_initial.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0006_require_contenttypes_0002'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Employee',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=300)),
+                ('start_date', models.DateField()),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='GroupProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('description', models.CharField(max_length=300)),
+                ('group', models.OneToOneField(to='auth.Group')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.CharField(max_length=300)),
+                ('time_stamp', models.DateField(auto_now=True)),
+                ('writer', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Type',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=300)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='groupprofile',
+            name='type',
+            field=models.ForeignKey(to='directory.Type'),
+        ),
+    ]
--- directory/models.py
+++ directory/models.py
@@ -5,24 +5,18 @@ class Type(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=300)
 
-	def __str__(self):
-        return self.name
-
 class GroupProfile(models.Model):
-	type = models.ForrignKey(Type)
+	type = models.ForeignKey(Type)
 	group = models.OneToOneField(Group)
-	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=300)
-
-	def __str__(self):
-        return self.name
 
 class Employee(models.Model):
 	user = models.OneToOneField(User)
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=300)
 	start_date = models.DateField()
-	picture = models.ImageField(upload_to=None)
 
-	def __str__(self):
-        return self.name
+class Message(models.Model):
+	writer = models.OneToOneField(User)
+	text = models.CharField(max_length=300)
+	time_stamp = models.DateField(auto_now=True)
