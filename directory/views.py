165e097597ecf5000f4402537afa73bbee848ce1
Vanessa Sergeon <vanessa.sergeon@magnetic.com>
2015-10-21 16:25:21 -0400

    bootstrap added

--- directory/views.py
+++ directory/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from .models import User, Employee
+from .models import User, Employee, GroupProfile
 
 
 def index(request):
     employees = Employee.objects.order_by('start_date')
+    groups = GroupProfile.objects.order_by('description')
     template = loader.get_template('directory/index.html')
     context = RequestContext(request, {
         'employees': employees,
+        'groups': groups,
     })
     return HttpResponse(template.render(context))
