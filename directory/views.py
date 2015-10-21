73cfba71de175496fb2a678719f684cbef8d23d1
Vanessa Sergeon <vanessa.sergeon@magnetic.com>
2015-10-21 15:09:01 -0400

    user name list rendering at /directory

--- directory/views.py
+++ directory/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from .models import User
+
+
+def index(request):
+    users = User.objects.order_by('username')
+    template = loader.get_template('directory/index.html')
+    context = RequestContext(request, {
+        'users': users,
+    })
+    return HttpResponse(template.render(context))
