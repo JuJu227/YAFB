b8998b4c92769adf0e9cb865f724d6ebabb20b1f
Vanessa Sergeon <vanessa.sergeon@magnetic.com>
2015-10-21 14:46:54 -0400

    models added to admin dashboard

--- directory/admin.py
+++ directory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Type)
+admin.site.register(GroupProfile)
+admin.site.register(Employee)
+admin.site.register(Message)
