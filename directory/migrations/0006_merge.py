fce5a6c7c5db711d8354a2e04461491dcad9ed93
Bo <bhuang1228@gmail.com>
2015-10-22 12:56:15 -0400

    merge from master

---
+++ directory/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('directory', '0005_employee_picture'),
+        ('directory', '0005_auto_20151022_1556'),
+    ]
+
+    operations = [
+    ]
