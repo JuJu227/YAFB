8d3bdb3d4917383e6c851a051b1166521d5234ff
Bo <bhuang1228@gmail.com>
2015-10-22 17:49:55 -0400

    finish the login/sign-up

--- directory/urls.py
+++ directory/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     url(r'^news-feed/', views.news_feed, name='news_feed'),
     url(r'^news-feed/post-item/$', views.post_item, name='post_item'),
 
-    url(r'^employee/message/(?P<employee_id>[0-9]+)/$', views.news_feed_by_empoyee, name='news_feed_by_empoyee'),
+    url(r'^employee/message/(?P<employee_id>[0-9]+)/$', views.news_feed_by_employee, name='news_feed_by_empoyee'),
     url(r'^employee/message/(?P<employee_id>[0-9]+)/newpost/$', views.post_news_feed, name='post_news_feed'),
     url(r'^login/$', views.login, name='login'),
     url(r'^signup/$', views.signup, name="signup"),
     url(r'^signin/$', views.signin, name="signin"),
+    url(r'^signout/$', views.signout, name="signout"),
 ]
--- directory/views.py
+++ directory/views.py
@@ -4,21 +4,25 @@ from django.template import RequestContext, loader
 from .models import User, Employee, GroupProfile, Type, Office, Message
 
 
-def nav():
+def nav(request):
     types = Type.objects.order_by('name')
     offices = Office.objects.order_by('name')
     template = loader.get_template('base.html')
     navinfo = {
         'types': types,
         'offices': offices,
+        'user_name': request.COOKIES.get("user_name"),
+        'user_id': request.COOKIES.get("user_id"),
     }
     return navinfo
+
 
 def login(request):
     template = loader.get_template('login.html')
     context = RequestContext(request, {})
 
     return HttpResponse(template.render(context))
+
 
 def signup(request):
     employee = Employee()
@@ -36,91 +40,130 @@ def signup(request):
 
     return HttpResponseRedirect("/login")
 
+
 def signin(request):
     email = "{username}".format(username=request.POST.get("username"))
     password = "{password}".format(password=request.POST.get("password"))
     employee = Employee.objects.filter(email=email.lower(), password=password)
 
-    print "Sign-in : %s %s --- %s" % (email, password, employee)
-
     if employee:
-        return HttpResponseRedirect("/directory")
+        redirect = HttpResponseRedirect("/directory");
+        redirect.set_cookie("login_user", True)
+        redirect.set_cookie("user_id", employee[0].id)
+        redirect.set_cookie("user_name", employee[0].full_name)
+
+        return redirect
 
     return HttpResponseRedirect("/login")
 
+
+def signout(request):
+    redirect = HttpResponseRedirect("/login")
+    redirect.delete_cookie("login_user")
+    redirect.delete_cookie("user_id")
+    redirect.delete_cookie("user_name")
+
+    return redirect
+
+
 def index(request):
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
     employees = Employee.objects.order_by('full_name')
     groups = GroupProfile.objects.order_by('group')
     template = loader.get_template('directory/index.html')
-    navinfo = nav()
+    navinfo = nav(request)
+
     context = RequestContext(request, {
         'employees': employees,
         'groups': groups,
         'header': "Directory",
         'navinfo': navinfo,
     })
+
     return HttpResponse(template.render(context))
 
+
 def employee_detail(request, employee_id):
-	employee = Employee.objects.get(pk=employee_id)
-	groups = GroupProfile.objects.filter(employee=employee_id)
-	template = loader.get_template('directory/employee.html')
-	navinfo = nav()
-	context = RequestContext(request, {
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
+    employee = Employee.objects.get(pk=employee_id)
+    groups = GroupProfile.objects.filter(employee=employee_id)
+    template = loader.get_template('directory/employee.html')
+    navinfo = nav(request)
+    context = RequestContext(request, {
         'employee': employee,
         'groups': groups,
         'navinfo': navinfo,
     })
 
-	return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context))
+
 
 def group_detail(request, group_id):
-	group = GroupProfile.objects.get(pk=group_id)
-	employees = Employee.objects.filter(groups=group_id)
-	header = "Group: %s" % group
-	template = loader.get_template('directory/group.html')
-	navinfo = nav()
-	context = RequestContext(request, {
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
+    group = GroupProfile.objects.get(pk=group_id)
+    employees = Employee.objects.filter(groups=group_id)
+    header = "Group: %s" % group
+    template = loader.get_template('directory/group.html')
+    navinfo = nav(request)
+    context = RequestContext(request, {
         'group': group,
         'employees': employees,
         'header': header,
         'navinfo': navinfo,
     })
-	
-	return HttpResponse(template.render(context))
+
+    return HttpResponse(template.render(context))
+
 
 def office_detail(request, office_id):
-	office = Office.objects.get(pk=office_id)
-	employees = Employee.objects.filter(office=office_id)
-	header = "Office: %s" % office
-	template = loader.get_template('directory/group.html')
-	navinfo = nav()
-	context = RequestContext(request, {
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
+    office = Office.objects.get(pk=office_id)
+    employees = Employee.objects.filter(office=office_id)
+    header = "Office: %s" % office
+    template = loader.get_template('directory/group.html')
+    navinfo = nav(request)
+    context = RequestContext(request, {
         'office': office,
         'employees': employees,
         'header': header,
         'navinfo': navinfo,
     })
-	
-	return HttpResponse(template.render(context))
+
+    return HttpResponse(template.render(context))
+
 
 def type_list(request, type_id):
-	groups = GroupProfile.objects.filter(type=type_id)
-	type = Type.objects.get(pk=type_id)
-	header = "Groups of type: %s" % type
-	template = loader.get_template('directory/type.html')
-	navinfo = nav()
-	context = RequestContext(request, {
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
+    groups = GroupProfile.objects.filter(type=type_id)
+    type = Type.objects.get(pk=type_id)
+    header = "Groups of type: %s" % type
+    template = loader.get_template('directory/type.html')
+    navinfo = nav(request)
+    context = RequestContext(request, {
         'groups': groups,
         'header': header,
         'navinfo': navinfo,
     })
 
-	return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context))
+
 
 def news_feed(request):
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
     messages = Message.objects.order_by('-time_stamp')
-    navinfo = nav()
+    navinfo = nav(request)
     template = loader.get_template('directory/news_item.html')
     context = RequestContext(request, {
         'newsfeed': messages,
@@ -129,18 +172,25 @@ def news_feed(request):
 
     return HttpResponse(template.render(context))
 
+
 def post_item(request):
-	# TODO: change employee to signed in user
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
+    # TODO: change employee to signed in user
     employee = Employee.objects.get(pk=2)
 
     message = Message(writer=employee, text=request.POST.get("content", ""))
-    message.save()
+    message.save(request)
     print "New message %s" % message.text
 
     return HttpResponseRedirect("..")
 
 
-def news_feed_by_empoyee(request, employee_id):
+def news_feed_by_employee(request, employee_id):
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
     employee = Employee.objects.get(pk=employee_id)
     messages = Message.objects.filter(writer=employee_id).order_by('-time_stamp')
 
@@ -152,7 +202,12 @@ def news_feed_by_empoyee(request, employee_id):
 
     return HttpResponse(template.render(context))
 
+
 def post_news_feed(request, employee_id):
+    if not __login__(request):
+        return HttpResponseRedirect("/login")
+
+    employee_id = request.COOKIES.get("user_id")
     employee = Employee.objects.get(pk=employee_id)
 
     if employee:
@@ -161,3 +216,13 @@ def post_news_feed(request, employee_id):
         print "New message %s" % message.text
 
     return HttpResponseRedirect("..")
+
+
+def __login__(request):
+    login_user = request.COOKIES.get("login_user")
+
+    if not login_user:
+        return False
+
+    return True
+
